68f3dfdedf5e61e275c0f721ce92fc261bddf97a
Juan Martin Loyola <jmloyola@outlook.com>
2013-09-25 23:26:02 -0300

    Prueba Interfaz
    Proyecto para probar como funcionan los distintos elementos de la
    interfaz

---
+++ PruebaInterfaz/src/pruebainterfaz/PruebaInterfaz.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebainterfaz;
+
+import InterfazGrafica.PantallaPrincipal;
+
+/**
+ *
+ * @author Juan
+ */
+public class PruebaInterfaz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        PantallaPrincipal pantalla = new PantallaPrincipal();
+        pantalla.setVisible(true);
+    }
+}
